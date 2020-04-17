a5f969ef3eaf5913fe248771522c1f16bddcf41a
Phoebe Law <phoebebasilio@gmail.com>
2020-04-16 19:24:18 -0700

    adds config and temp styles

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-  API_KEY: process.env.REACT_APP_API_KEY,
+  API_ENDPOINT: process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_ENDPOINT
+    : 'http://localhost:8000/api',
+    TOKEN_KEY: process.env.TOKEN_KEY,
 }
