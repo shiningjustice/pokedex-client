656401e364f7c6e19443c3ce9f587db01472838b
Phoebe Law <phoebebasilio@gmail.com>
2020-04-16 22:56:22 -0700

    adds bootstrap file for easy exporting

---
+++ src/components/Bootstrap.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+export {
+  Button,
+  Modal,
+  Container,
+  Row
+}
