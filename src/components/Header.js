55dbb1cb8a30da43bb10c8ee24e689625eb91739
Phoebe Law <phoebebasilio@gmail.com>
2020-04-17 00:55:05 -0700

    bootstraps header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { useAuth } from './helpers/context';
+import { Container, Button } from './Bootstrap';
 
 import './tempstyles.css';
 
@@ -10,20 +11,39 @@ const Header = (props) => {
 
 	const renderUserLinks = () => {
 		if (!!auth.user.id) {
-			return <Link to='/login'>Logout</Link>;
+			return (
+				<Link to='/'>
+					<Button variant='light'>Logout</Button>
+				</Link>
+			);
 		} else {
-			return <Link to='/login'>Login</Link>;
+			return (
+				<>
+					<Link to='/login' className='mr-2'>
+						<Button variant='light'>Login</Button>
+					</Link>
+					<Link to='/register'>
+						<Button variant='light'>Register</Button>
+					</Link>
+				</>
+			);
 		}
 	};
 
 	return (
-		<div className='Header'>
+		<Container fluid className='Header d-flex align-items-center'>
 			<Link to='/' className='Header Link'>
 				<h1>Pokedex</h1>
 			</Link>
-			<Link to='/favorites'>Heart Icon</Link>
+			<Link className='ml-auto mr-2' to='/favorites'>
+				<Button variant='light'>
+					<span role='img' aria-label='heart'>
+						❤
+					</span>
+				</Button>
+			</Link>
 			{renderUserLinks()}
-		</div>
+		</Container>
 	);
 };
 
